ce6a624bfe84f0e6f17748b16baba96c7ed7240e
Joshua J. Bouw <joshua@aurora.dev>
2021-11-29 15:11:02 +0400

    Change dist -> bin folder

--- engine-tests/src/test_utils/mod.rs
+++ engine-tests/src/test_utils/mod.rs
@@ -488,9 +488,9 @@ impl Default for AuroraRunner {
     fn default() -> Self {
         let aurora_account_id = "aurora".to_string();
         let evm_wasm_bytes = if cfg!(feature = "mainnet-test") {
-            std::fs::read("../dist/mainnet-development.wasm").unwrap()
+            std::fs::read("../bin/mainnet-development.wasm").unwrap()
         } else if cfg!(feature = "testnet-test") {
-            std::fs::read("../dist/testnet-development.wasm").unwrap()
+            std::fs::read("../bin/testnet-development.wasm").unwrap()
         } else {
             panic!("AuroraRunner requires mainnet-test or testnet-test feature enabled.")
         };
